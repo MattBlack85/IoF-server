cb9ef629b6910dc9f2f0344f610843d9d2b8034b
Mattia Procopio <promat85@gmail.com>
2017-03-14 11:21:13 +0100

    Add main app

---
+++ iof.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import json
+import re
+import subprocess
+
+from flask import Flask, abort, render_template, request, url_for
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    url_for("static", filename="main.css")
+    url_for("static", filename="main.js")
+    return render_template("index.html")
+
+
+@app.route("/temp", methods=["GET"])
+def temp():
+    if request.is_xhr:
+        temp = subprocess.check_output(["check_temperature"])
+        if re.match("\d\d.\d\d\d\\n", temp):
+            return json.dumps({"temperature": temp.strip("\n")})
+        return json.dumps({"error": "reading_error"})
+
+    abort(401)
